2a6b9923d2fc7b150ed22b944a90ffd4e2051e43
john <john@n1-31-68.dhcp.drexel.edu>
2018-04-23 21:29:02 -0400

    format

--- kernels2.py
+++ kernels2.py
@@ -2,28 +2,32 @@
 import KMC
 import statevector as sv
 
+
 class MergeKernel:
-    def __init__(self,frequency,**kwargs):
-        self.frequency=frequency
+    def __init__(self, frequency, **kwargs):
+        self.frequency = frequency
         try:
-            self.fractal_dimension=kwargs["fractal_dimension"]
+            self.fractal_dimension = kwargs["fractal_dimension"]
         except:
-            self.fractal_dimension=1.0/3.0
-    def __call__(self,r,s):
+            self.fractal_dimension = 1.0/3.0
+
+    def __call__(self, r, s):
         return 1.0*self.frequency*(r**(self.fractal_dimension)+s**(self.fractal_dimension))
 
+
 class Coagulation:
-    def __init__(self,f,nc,*args,**kwargs):
-        self.freq=f
-        self.index_pairs=[]
-    def __call__(self,s):
+    def __init__(self, f, nc, *args, **kwargs):
+        self.freq = f
+        self.index_pairs = []
+
+    def __call__(self, s):
         try:
-            outp=[]
-            print("THE SSSSS ",s)
-            if len(s)>2:
-                for ij,j in enumerate(s[1:]):
-                    for ii,i in enumerate(s[ij:]):
-                        self.index_pairs.append((ii+1,ij+ii+1))
+            outp = []
+            print("THE SSSSS ", s)
+            if len(s) > 2:
+                for ij, j in enumerate(s[1:]):
+                    for ii, i in enumerate(s[ij:]):
+                        self.index_pairs.append((ii+1, ij+ii+1))
                         outp.append(self.freq)
                 return outp
             else:
@@ -31,58 +35,66 @@ class Coagulation:
         except:
             return 0.0
 
+
 class Fragmentation:
-    def __init__(self,f,nc,*args,**kwargs):
-        self.freq=f
-    def __call__(self,s):
+    def __init__(self, f, nc, *args, **kwargs):
+        self.freq = f
+
+    def __call__(self, s):
         try:
-            if len(s)>1:
+            if len(s) > 1:
                 for i in s[1:]:
-                    if i>3:
-                        return [(j-3)*self.freq for j in range(4,i)]
+                    if i > 3:
+                        return [(j-3)*self.freq for j in range(4, i)]
                     else:
                         return 0.0
             else:
                 return 0.0
         except:
-            return 0.0                       
+            return 0.0
+
 
 class MonomerAddition:
-    def __init__(self,f,nc,*args,**kwargs):
-        self.freq=f
-        self.nc=nc
-    def __call__(self,s):
+    def __init__(self, f, nc, *args, **kwargs):
+        self.freq = f
+        self.nc = nc
+
+    def __call__(self, s):
         try:
-            if s[0]>0 and len(s)>1:
-                return [s[i]*s[0]*self.freq for i in range(1,len(s))]
+            if s[0] > 0 and len(s) > 1:
+                return [s[i]*s[0]*self.freq for i in range(1, len(s))]
             else:
                 return 0.0
         except:
             return 0.0
 
+
 class Nucleation:
-    def __init__(self,f,nc):
-        self.freq=f
-        self.nc=nc
-    def __call__(self,s,*args,**kwargs):
-        prod=self.freq
+    def __init__(self, f, nc):
+        self.freq = f
+        self.nc = nc
+
+    def __call__(self, s, *args, **kwargs):
+        prod = self.freq
         try:
-            if s[0]>self.nc:
-                print(3)   
+            if s[0] > self.nc:
+                print(3)
                 for i in range(self.nc):
-                    prod=1.0*prod*(1.0*s[0]-1.0*i)
+                    prod = 1.0*prod*(1.0*s[0]-1.0*i)
                 return prod
             else:
                 return 0.0
         except:
             return 0.0
 
+
 class MonomerSubtraction:
-    def __init__(self,f,*args,**kwargs):
-        self.freq=f
-    def __call__(self,s):
+    def __init__(self, f, *args, **kwargs):
+        self.freq = f
+
+    def __call__(self, s):
         try:
-            if len(s)>1:
+            if len(s) > 1:
                 return [2*self.freq for i in range(len(s)-1)]
             else:
                 return 0.0
